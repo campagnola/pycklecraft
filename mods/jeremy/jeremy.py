43aedcb10f4b7360c8a55a0aacf2203a4b5b9e62
Jeremy Lightsmith <jeremy.lightsmith@gmail.com>
2021-06-27 05:28:13 -0700

    updating events

--- mods/jeremy/jeremy.py
+++ mods/jeremy/jeremy.py
@@ -1,7 +1,9 @@
-import pycklecraft
+import sys
+sys.path.append('.')
 import pycklecraft
 
-p = pycklecraft.PicklecraftClient('flarion.local', verbose=True)
+# p = pycklecraft.PicklecraftClient('flarion.local', verbose=True)
+p = pycklecraft.PicklecraftClient('localhost', verbose=True)
 
 # players = p.players
 # print("Players:")
@@ -9,7 +11,7 @@ p = pycklecraft.PicklecraftClient('flarion.local', verbose=True)
 #     print(player.name)
 # player = players[0]
 
-# p.lift_boot()
+p.lift_boot()
 p.set_day_time('day')
 # print("First player:", p.player(player.name).data)
 
@@ -17,11 +19,30 @@ p.set_day_time('day')
 
 # p.place_block('grass', [player.x, player.y, player.z])
 
+ice_player = None
 
-def on_command(cmd):
-    print("CMD:", cmd)
+def on_event(event):
+    global ice_player
+    player = event.player
+    
+    if event.type == 'player_move_event':
+        if player.name == ice_player:
+            p.place_block('ice', [player.x, player.y - 1, player.z])
 
+    elif event.type == 'command_event':
+        print("command: ", event.data)
+        if event.command == '/jump':
+            p.move_player(player.name, [player.x + 1, player.y, player.z])
+        
+        elif event.command == '/ice':
+            if ice_player == player.name:
+                ice_player = None
+            else:
+                ice_player = player.name
+p.set_on_command(on_event)
 
-p.set_on_command(on_command)
+@p.on_player_move
+def on_player_move(event):
+
 
 p.wait_for_events()
--- pycklecraft/event.py
+++ pycklecraft/event.py
@@ -4,9 +4,12 @@ from .player import Player
 class Event:
     def __init__(self, data):
         self.data = data
-        self.event = data['event']
-        self.command = data['command']
+        self.type = data['type']
 
     @property
     def player(self):
         return Player(self.data['player'])
+
+    @property
+    def command(self):
+        return self.data['command']
