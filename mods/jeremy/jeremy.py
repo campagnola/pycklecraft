37f8c5634ea3ac7fe558bbe9b82b4be6d12c99dd
Jeremy Lightsmith <jeremy.lightsmith@gmail.com>
2021-06-27 06:24:38 -0700

    mc not p

--- mods/jeremy/jeremy.py
+++ mods/jeremy/jeremy.py
@@ -3,36 +3,36 @@ sys.path.append('.')
 import pycklecraft
 
 # p = pycklecraft.PicklecraftClient('flarion.local', verbose=True)
-p = pycklecraft.PicklecraftClient('localhost', verbose=True)
+mc = pycklecraft.PicklecraftClient('localhost', verbose=True)
 
-# players = p.players
+# players = mc.players
 # print("Players:")
 # for player in players:
 #     print(player.name)
 # player = players[0]
 
-p.lift_boot()
-p.set_day_time('day')
-# print("First player:", p.player(player.name).data)
+mc.lift_boot()
+mc.set_day_time('day')
+# print("First player:", mc.player(player.name).data)
 
-# print("Entities:", p.nearby_entities(player.name, 50))
+# print("Entities:", mc.nearby_entities(player.name, 50))
 
-# p.place_block('grass', [player.x, player.y, player.z])
+# mc.place_block('grass', [player.x, player.y, player.z])
 
-# player = p.player('jeremylightsmith')
-# p.place_block('ice', [player.x, player.y - 1, player.z])
+# player = mc.player('jeremylightsmith')
+# mc.place_block('ice', [player.x, player.y - 1, player.z])
 
 ice_player = None
 
-@p.on_event('player_move_event')
+@mc.on_event('player_move_event')
 def on_player_move(event):
     player = event.player
     
     print(player.name, " moved ", player.position, "(ice player = ", ice_player, ")")
     if player.name == ice_player:
-        p.place_block('ice', [player.x, player.y - 1, player.z])
+        mc.place_block('ice', [player.x, player.y - 1, player.z])
 
-@p.on_command('/ice')
+@mc.on_command('/ice')
 def on_ice(event):
     global ice_player
     player = event.player
@@ -42,9 +42,9 @@ def on_ice(event):
     else:
         ice_player = player.name
 
-@p.on_command('/iron')
+@mc.on_command('/rail')
 def on_iron(event):
     player = event.player
-    p.place_block('iron_block', [player.x, player.y-1, player.z])
+    mc.place_block('iron_block', [player.x, player.y-1, player.z])
 
-p.wait_for_events()
+mc.wait_for_events()
