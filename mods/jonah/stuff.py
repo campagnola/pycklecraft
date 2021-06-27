b10dab456383bf82e210180775c4adf557138884
phoenix-90 <jonah.campagnola@gmail.com>
2021-06-27 12:31:21 -0700

    upgrading to new api

--- mods/jonah/stuff.py
+++ mods/jonah/stuff.py
@@ -3,70 +3,56 @@ sys.path.append('.')
 import pycklecraft
 import threading
 
-p = pycklecraft.PicklecraftClient('flarion.local', verbose=True)
+mc = pycklecraft.PicklecraftClient('flarion.local', verbose=True)
 
-p.set_day_time('day')
+mc.set_day_time('day')
 
 def cage(type, position):
     x, y, z = position
-    p.place_blocks(type, [x-2, y, z-2], [x+2, y+2, z-2])
-    p.place_blocks(type, [x-2, y, z+2], [x+2, y+2, z+2])
-    p.place_blocks(type, [x-2, y, z-2], [x-2, y+2, z+2])
-    p.place_blocks(type, [x+2, y, z-2], [x+2, y+2, z+2])
-    p.place_blocks(type, [x-2, y+2, z-2], [x+2, y+2, z+2])
+    mc.place_blocks(type, [x-2, y, z-2], [x+2, y+2, z-2])
+    mc.place_blocks(type, [x-2, y, z+2], [x+2, y+2, z+2])
+    mc.place_blocks(type, [x-2, y, z-2], [x-2, y+2, z+2])
+    mc.place_blocks(type, [x+2, y, z-2], [x+2, y+2, z+2])
+    mc.place_blocks(type, [x-2, y+2, z-2], [x+2, y+2, z+2])
 
 tunta_running = False
 tunta_player = None
 
-def tunta_run():
-    while True:
-        if tunta_running:
-            player = p.player(tunta_player.name)
-            p.place_block('redstone_block', 
-                    [player.x,player.y - 1,player.z])
+@mc.on_event('player_move_event')
+def on_player_move(event):
+    player = event.player
+    if tunta_running and player.name == tunta_player:
+        mc.place_block('redstone_block', [player.x,player.y - 1,player.z])
 
-def on_command(event):
+@mc.on_command('/cage')
+def on_cage(player, command):
+    cage('diamond_block', player.position)
+
+@mc.on_command('/uncage')
+def on_uncage(player, command):
+    cage('air', player.position)
+
+@mc.on_command('/rock')
+def on_rock(player, command):
+    mc.place_blocks_in_line(
+        'cobblestone', 
+        player.position, 
+        player.rotation[1], 
+        20
+    )
+
+@mc.on_command('/red')
+def start_red(player, command):
     global tunta_player
     global tunta_running
-    print("event:", event)
-    if event.command == "/cage":
-        cage('diamond_block', event.player.position)
     
-    elif event.command == "/uncage":
-        cage('air', event.player.position)
+    tunta_player = player
+    tunta_running = True
 
-    elif event.command == "/rock":
-        p.place_blocks_in_line(
-            'cobblestone', 
-            event.player.position, 
-            event.player.rotation[1], 
-            20
-        )
+@mc.on_command('/redd')
+def stop_red(player, command):
+    global tunta_running
 
-    elif event.command == "/red":
-        tunta_player = event.player
-        tunta_running = True
+    tunta_running = False
 
-
-
-
-
-
-    elif event.command == "/redd":
-        tunta_running = False
-
-
-
-
-
-
-
-p.set_on_command(on_command)
-
-threading.Thread(target=tunta_run, daemon=True).start()
-
-print('cages are ready!')
-p.wait_for_events()
-
-
-
+mc.wait_for_events()
